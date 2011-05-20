98fb511be8924cd3b2429d6ee8c4143c02aa85db
Jen-Mei Wu <jenmei@colorxp.com>
2011-05-20 10:29:36 -0700

    Need to use rake 0.8.7. If older version exits in path, use bundle exec rake.

--- db/migrate/20110520170819_create_screenshots.rb
+++ db/migrate/20110520170819_create_screenshots.rb
@@ -4,7 +4,7 @@ class CreateScreenshots < ActiveRecord::Migration
       t.string :title
       t.text :description
       t.string :image_file_name
-      t.image_content_type :
+      t.string :image_content_type
       t.integer :image_file_size
       t.datetime :image_uploaded_at
 
