433899611648317f0717333a3c3ee4b822cd64cb
Jen-Mei Wu <jenmei@colorxp.com>
2011-05-20 09:20:09 -0700

    initial commit after rails new -J -T and bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JqueryFileuploadTest::Application.config.secret_token = 'c26e8037dd079c0ba109fb38969bb68e90b94fe6d55937631d1097ceb2d88f3e8fa0afe8aa82646b03bd6a9bc697b304d3cc0e9772171be66a2d7c1c8a408bfe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+JqueryFileuploadTest::Application.config.session_store :cookie_store, :key => '_jquery_fileupload_test_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# JqueryFileuploadTest::Application.config.session_store :active_record_store
